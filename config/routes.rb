9d50cacab70616627d54575b52beb9c1e5a68b97
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-07-20 07:32:06 -0700

    update migrations, update routes and create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  resources :ingredients
-  resources :comments
+
+  resources :ingredients, only: [:show, :index]
   resources :cocktail_recipes
-  resources :categories
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :categories, only: [:show, :index]
+  
+  root to: 'home#index' 
+  get '/home/index', to: 'home#index' 
+
+  resources :users, only: [:show] do 
+    resources :cocktail_recipes, only: [:show, :index, :new]
+  end 
+
+  resources :cocktail_recipes do
+    resources :comments
+  end
+  
 end
--- db/migrate/20200720134330_create_cocktail_recipes.rb
+++ db/migrate/20200720134330_create_cocktail_recipes.rb
@@ -1,7 +1,13 @@
 class CreateCocktailRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :cocktail_recipes do |t|
-
+      t.integer :user_id 
+      t.integer :category_id 
+      t.string :title 
+      t.text :description 
+      t.text :directions 
+      t.attachment :avatar 
+      t.integer :comments_count
       t.timestamps
     end
   end
