73e35745e2c4c68e0e5a0e7b48da08ee31dc5e9a
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-07-20 09:55:04 -0700

    update cocktail recipe controller and create form partials

--- app/controllers/cocktail_recipes_controller.rb
+++ app/controllers/cocktail_recipes_controller.rb
@@ -1,2 +1,66 @@
-class CocktailRecipesController < ApplicationController
+class CocktailRecipesController < ApplicationController 
+
+    before_action :find_cocktail_recipe, only: [:show, :edit, :update, :destroy] 
+    before_action :authenticate_user!
+
+    def new 
+        @cocktail_recipe = CocktailRecipe.new 
+        @comment = Comment.new 
+    end  
+
+    def create
+      @cocktail_recipe = current_user.cocktail_recipes.new(cocktail_recipe_params) 
+      if @cocktail_recipe.save
+        redirect_to cocktail_recipe_path(@cocktail_recipe), notice: "Your recipe has successfully been added" 
+      else  
+        render :new
+      end
+    end
+    
+    def show   
+      @comment = current_user.comments.build(cocktail_recipe: @cocktail_recipe)
+    end 
+
+    def index 
+      @users = User.all 
+        if params[:search]
+          @cocktail_recipes = CocktailRecipe.search(params[:search]).order("created_at DESC")
+        elsif !params[:user].blank?
+          @cocktail_recipes = CocktailRecipe.by_user(params[:user]).order(:title)
+        elsif params[:user_id]
+          @cocktail_recipes = User.find(params[:user_id]).cocktail_recipes.order('title ASC')
+        else
+          @cocktail_recipes = CocktailRecipe.all.order(:title)
+        end
+    end 
+
+
+    def edit  
+    end
+
+    def destroy 
+        @cocktail_recipe.destroy
+        redirect_to cocktail_recipes_url
+      end
+     
+    def update
+        if @cocktail_recipe.update(cocktail_recipe_params)
+          redirect_to @cocktail_recipe, notice: "Your recipe has successfully been updated"
+        else 
+          redirect_to new_cocktail_recipe_path
+        end
+      end
+
+   private
+
+   def cocktail_recipe_params
+     params.require(:cocktail_recipe).permit(:user_id, :category_name, :title, :directions, :description, :avatar, :avatar_delete, :add_ingredient, :remove_ingredient, :ingredients_attributes => [:id, :name]) 
+   end 
+
+
+   def find_cocktail_recipe
+    @cocktail_recipe = CocktailRecipe.find(params[:id])
+   end
+    
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_150117) do
+ActiveRecord::Schema.define(version: 2020_07_20_161016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_07_20_150117) do
     t.datetime "avatar_updated_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "comments_count"
   end
 
   create_table "comments", force: :cascade do |t|
